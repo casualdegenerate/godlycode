5b9d4ee682200ff9f75ab5d59997e0b199cdaeac
casual_degenerate <46980628+casualdegenerate@users.noreply.github.com>
2020-11-03 18:33:38 -0500

    Update cmds.lua

--- cmds.lua
+++ cmds.lua
@@ -922,13 +922,13 @@ getgenv().Commands = {
             while antifling do
                 local h = game:GetService("Players").LocalPlayer.Character:WaitForChild("HumanoidRootPart",2)
                 if h then
-                    if h.Velocity.X > 500 or h.Velocity.Y > 500 or h.Velocity.Z > 500 then
+                    if h.Velocity.X > 100 or h.Velocity.Y > 100 or h.Velocity.Z > 100 then
                         h.Anchored = true
                         h.Velocity = Vector3.new(0,0,0)
                         wait(.3)
                         h.Anchored = false
                     end
-                    if h.Velocity.X < -500 or h.Velocity.Y < -500 or h.Velocity.Z < -500 then
+                    if h.Velocity.X < -100 or h.Velocity.Y < -100 or h.Velocity.Z < -100 then
                         h.Anchored = true
                         h.Velocity = Vector3.new(0,0,0)
                         wait(.3)
