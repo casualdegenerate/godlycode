b68817116f60c47ddc50a1469956fff809100fcf
casual_degenerate <46980628+casualdegenerate@users.noreply.github.com>
2021-02-25 21:26:56 -0500

    Fuck you too

--- cmds.lua
+++ cmds.lua
@@ -1,7 +1,7 @@
 repeat wait() until game:GetService("ReplicatedStorage"):FindFirstChild("DefaultChatSystemChatEvents")
 repeat wait() until game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents:FindFirstChild("SayMessageRequest")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/casualdegenerate/cd/master/Better%20Proto%20API"))()
-if not rconsoleprint then
+if not syn then
     lchat("Run it on SynX you idiot. Only works on SynX.",Color3.new(1,0,0))
     rchat("music 5648499584")
     return
