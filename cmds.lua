f4b1a4920de13644a5dde09a47e6e33d72229fe1
casual_degenerate <46980628+casualdegenerate@users.noreply.github.com>
2021-06-20 18:46:13 -0500

    Maybe fixed?

--- cmds.lua
+++ cmds.lua
@@ -1,3 +1,10 @@
+--[[
+If you remove credit, you will most likely be shat on because of errors from here.
+Please go to my server and ask my account about the error since I'd rather not accept friend request nor people talking to me on my main discord.
+report bugs here: https://discord.gg/4pphCyUVBf
+Otherwise, enjoy the script!
+(seriously just report if it does not work, I'm a good scripter ok, I just want to make cool stuff)
+--]]
 game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents")
 game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest")
 print("start")
@@ -7,6 +14,93 @@ if not syn then
     return
 end
 print("pass syn check")
+
+-- // This is just a library, so incase you don't have it...
+
+loadstring(
+	[=====[
+--LUAU ROBLOX LUA
+
+--SynXLuaU
+--[[ Deprecated for now.
+if rconsoleprint then
+	--messagebox("Hello SynX user.","cd/library",0)
+	
+end
+--]]
+--EXTERNAL LUAU
+if getgenv then
+
+	-- // SERVICES
+	
+	getgenv().Players = game:GetService("Players")
+	getgenv().FirstReplicated = nil
+	
+	
+	
+	-- // #############
+
+	--Easy to write coroutine.
+	getgenv().fspawn = function(f)
+		coroutine.wrap(f) -- / 5.24.2021 Doing a wrap might work better?
+	end
+	--Easy to write frame wait.
+	getgenv().fwait=function()
+		game:GetService("RunService").RenderStepped:Wait()
+	end
+	--This should just be a function it's self in Lua.
+	getgenv().stringtobyte = function(text)
+		local output = ""
+		for i=1,#text do
+			output = output.."\\"..text:sub(i):byte()
+		end
+		return output
+	end
+	--This is the say request remote and won't fire any .Chatted connections. So pretty much most basic admin scripts(This includes chat loggers).
+	getgenv().fchat = function(input)
+		game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest"):FireServer(input,"All")
+	end
+	--Real chat(works in vanilla games that don't mess with the .Chatted or say request remote.
+	getgenv().rchat = function(input)
+		if chatbypass then 
+			getgenv().chatbypass = false
+			game:GetService("Players"):Chat(input) 
+			getgenv().chatbypass = true
+		elseif _G.owoToggle then
+			_G.owoToggle = false
+			game:GetService("Players"):Chat(input) 
+			_G.owoToggle = true
+		end
+	end
+	getgenv().tchat = function(input)
+		fchat(input)
+		rchat(input)
+	end
+	--This is proof you are using my library(do you like the way I worded that >3<)
+	getgenv().is_cd_caller = function()
+		return true
+	end
+	
+	-- / This is WaitFor it's a form of WaitForChild() that will only take strings of code and convert them to wait for each instance.
+	getgenv().WaitFor = function(objectString)
+		splits = objectString:split(".")
+		local out = splits[1]
+		for i,v in next, splits do
+			if i~=1 then out = out .. ":WaitForChild('" .. v .. "')" end
+		end
+		return out
+	end
+	
+	-- / Rejoin
+	getgenv().Rejoin = function()
+		game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId)
+	end
+
+end
+]=====])()
+
+--############
+
 local JSOND = function(a)return game:GetService("HttpService"):JSONDecode(a)end
 local JSONE = function(a)return game:GetService("HttpService"):JSONEncode(a)end
 JSONB=function(jsn) --Thank you [NekO]
